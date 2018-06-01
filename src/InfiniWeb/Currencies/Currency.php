50835abbd12c6044140b9aadfb67f0a8b887eb9a
infiniweb <17177824+infiniweb@users.noreply.github.com>
2018-06-01 07:56:29 +0400

    Currency squeleton

--- src/InfiniWeb/Currencies/Currency.php
+++ src/InfiniWeb/Currencies/Currency.php
@@ -3,5 +3,26 @@ namespace InfiniWeb\Currencies;
 
 class Currency
 {
+	protected $isoCode;
+	protected $exchangeRate;
+
+	public function __construct($isoCode = null)
+	{
+		if ($isoCode === null) {
+			// new
+		} else {
+			// todo - load from ISO code
+		}
+	}
+
+	public function getIsoCode()
+	{
+		return $this->isoCode;
+	}
+
+	public function getExchangeRate()
+	{
+		return $this->exchangeRate;
+	}
 
 }
